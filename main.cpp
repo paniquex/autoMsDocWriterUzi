061af18bbca8ebdb874c70fb412f744a5a3b7cc4
paniquex <paniquexx@gmail.com>
2018-08-02 18:39:51 +0500

    Добавил ввод ФИО, даты рождения. Автоматическая установка даты исследования, Добавил запись про печень.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <QApplication>
 #include <QtWidgets>
 #include <QLineEdit>
+#include <QFrame>
+#include "patient.h"
+//#include "livercheck.h"
 
 int main(int argc, char *argv[])
 {
@@ -9,14 +12,54 @@ int main(int argc, char *argv[])
     QWidget wgt;
 
     QLineEdit* patientName = new QLineEdit;
-    QLabel* labelName = new QLabel( "ФИО пациента: " );
+    QDateEdit* patientAge = new QDateEdit;
+    QDateEdit* researchDate = new QDateEdit( QDate::currentDate() );
+    patientName->setFixedSize( 300, 20 );
 
-    QVBoxLayout* mainLayout = new QVBoxLayout;
+    Patient* Kolya = new Patient;
+    Kolya->setName( "Kolya Efimov" );
+// Создаение Заголовков
+
+    QLabel* labelName = new QLabel( "ФИО: "  );
+    QLabel* labelAge  = new QLabel( "Год Рождения: " );
+    QLabel* labelDate = new QLabel( "Дата исследования: " );
+
+
+    QGridLayout* mainLayout = new QGridLayout();
+
     QHBoxLayout* nameLayout = new QHBoxLayout;
     nameLayout->addWidget( labelName );
     nameLayout->addWidget( patientName );
-    mainLayout->addLayout( nameLayout );
+
+    QHBoxLayout* ageLayout = new QHBoxLayout;
+    ageLayout->addWidget( labelAge );
+    ageLayout->addWidget( patientAge );
+
+    QHBoxLayout* dateLayout = new QHBoxLayout;
+    dateLayout->addWidget( labelDate );
+    dateLayout->addWidget( researchDate );
+
+//Основной слой
+    QFrame mainFrame;
+    mainFrame.setFrameShape( QFrame::Box );
+    mainLayout->addWidget( &mainFrame, 1, 1, 3, 2 );
+
+    mainLayout->addLayout( nameLayout, 1, 1 );
+    mainLayout->addLayout( ageLayout, 2, 1 );
+    mainLayout->addLayout( dateLayout, 3, 1 );
+
     wgt.setLayout( mainLayout );
+
+//Начало протокола:
+    QFrame protocallFrame;
+    protocallFrame.setFrameShape( QFrame::Box );
+    QLabel* protocallLabel = new QLabel( "\t\t\t ПРОТОКОЛ \n ультразвукогого исследования органов брюшной полости");
+    mainLayout->addWidget( &protocallFrame, 3, 1, 3, 2 );
+    mainLayout->addWidget( protocallLabel, 4, 1);
+
+//Заплняемая часть
+    QLabel* liverLabel = new QLabel( "Печень: визуализируется");
+    mainLayout->addWidget( liverLabel, 5, 1 );
 
     wgt.show();
 
