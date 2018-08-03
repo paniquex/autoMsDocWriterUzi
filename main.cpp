443448a3c3e975f5f87cd8892878190b7cf0d3c8
paniquex <paniquexx@gmail.com>
2018-08-03 09:07:41 +0500

    Added ballblader block, replace all layouts to separate part of code

--- main.cpp
+++ main.cpp
@@ -27,39 +27,8 @@ int main(int argc, char *argv[])
     QLabel* labelDate = new QLabel( "Дата исследования: " );
 
 
-    QGridLayout* mainLayout = new QGridLayout();
-
-    QHBoxLayout* nameLayout = new QHBoxLayout;
-    nameLayout->addWidget( labelName );
-    nameLayout->addWidget( patientName );
-
-    QHBoxLayout* ageLayout = new QHBoxLayout;
-    ageLayout->addWidget( labelAge );
-    ageLayout->addWidget( patientAge );
-
-    QHBoxLayout* dateLayout = new QHBoxLayout;
-    dateLayout->addWidget( labelDate );
-    dateLayout->addWidget( researchDate );
-
-//Основной слой
-    QFrame mainFrame;
-    mainFrame.setFrameShape( QFrame::Box );
-    mainLayout->addWidget( &mainFrame, 1, 1, 3, 3 );
-
-    mainLayout->addLayout( nameLayout, 1, 1 );
-    mainLayout->addLayout( ageLayout, 2, 1 );
-    mainLayout->addLayout( dateLayout, 3, 1 );
-
-//Начало протокола:
-    QFrame liverFrame;
-    liverFrame.setFrameShape( QFrame::Box );
-    QLabel* protocallLabel = new QLabel( "\t\t\t ПРОТОКОЛ \n ультразвукогого исследования органов брюшной полости");
-    mainLayout->addWidget( &liverFrame, 4, 1, 14, 3 );
-    mainLayout->addWidget( protocallLabel, 4, 1);
-
 //Заплняемая часть
     QLabel* liverLabel = new QLabel( "ПЕЧЕНЬ: визуализируется");
-    mainLayout->addWidget( liverLabel, 5, 1 );
     //Расположение печени
 
     QLabel* placeLabel = new QLabel( "Расположена: " );
@@ -70,7 +39,6 @@ int main(int argc, char *argv[])
     placeLayout->addWidget( placeLabel );
     placeLayout->addWidget( placeStdRadio );
     placeLayout->addWidget( placeLeftRadio );
-    mainLayout->addLayout( placeLayout, 5, 2, 2, 1);
 
     //Доли
     QCheckBox* leftShareCheck = new QCheckBox( "Левая доля: не увеличена");
@@ -83,8 +51,6 @@ int main(int argc, char *argv[])
                       [&] { if ( !( rightShareCheck->isChecked() ) ) rightShareCheck->setText("Правая доля: не увеличена");
                             if ( ( rightShareCheck->isChecked() ) ) rightShareCheck->setText("Правая доля: увеличена");
                             });
-    mainLayout->addWidget( leftShareCheck, 8, 1 );
-    mainLayout->addWidget( rightShareCheck, 9, 1 );
 
     //Толщина
     QSpinBox* leftThickSpin = new QSpinBox;
@@ -97,30 +63,18 @@ int main(int argc, char *argv[])
     rightThickSpin->setRange( 0, 300 );
     leftThickSpin->clear();
     rightThickSpin->clear();
-    mainLayout->addWidget( thickLabel1, 8, 2 );
-    mainLayout->addWidget( thickLabel2, 9, 2 );
-    mainLayout->addWidget( leftThickSpin, 8, 3 );
-    mainLayout->addWidget( rightThickSpin, 9, 3 );
 
     //Контуры
     QLabel* contourLabel = new QLabel( "Контуры: " );
     QRadioButton* contourStraight = new QRadioButton( "Ровные" );
     QRadioButton* contourNotStraight = new QRadioButton( "Неровные" );
     QRadioButton* contourHilly = new QRadioButton( "Бугристые" );
-    QHBoxLayout* contourLayout = new QHBoxLayout;
-
         //делаем независимыми c предыдущими радио-кнопками
     QButtonGroup* contourGroups = new QButtonGroup;
     contourGroups->addButton( contourStraight );
     contourGroups->addButton( contourNotStraight );
     contourGroups->addButton( contourHilly );
     contourGroups->setExclusive( true );
-
-    contourLayout->addWidget( contourLabel );
-    contourLayout->addWidget( contourStraight );
-    contourLayout->addWidget( contourNotStraight );
-    contourLayout->addWidget( contourHilly );
-    mainLayout->addLayout( contourLayout, 10, 1, 1, 1 );
 
     //Эхоструктура
     QCheckBox* echoCheck = new QCheckBox( "Эхоструктура: неоднородная" );
@@ -129,7 +83,7 @@ int main(int argc, char *argv[])
                           [&] { if ( !( echoCheck->isChecked() ) ) echoCheck->setText("Эхоструктура: неоднородная");
                                 if ( ( echoCheck->isChecked() ) ) echoCheck->setText("Эхоструктура: однородная");
                                 });
-    mainLayout->addWidget( echoCheck, 10, 2 );
+
 
     //Эхогенность
     QLabel* echogenLabel = new QLabel( "Эхогенность: " );
@@ -143,12 +97,6 @@ int main(int argc, char *argv[])
     echogenGroup->addButton( echogenDownRadio );
     echogenGroup->addButton( echogenMidRadio );
     echogenGroup->setExclusive( true );
-    QHBoxLayout* echogenLayout = new QHBoxLayout;
-    echogenLayout->addWidget( echogenLabel );
-    echogenLayout->addWidget( echogenUpRadio );
-    echogenLayout->addWidget( echogenMidRadio );
-    echogenLayout->addWidget( echogenDownRadio );
-    mainLayout->addLayout( echogenLayout, 11, 1, 1, 1 );
 
     //Магистр. ствол
     QCheckBox* magCheck = new QCheckBox( "Магистральный ствол воротной вены: не расширен, " );
@@ -159,10 +107,6 @@ int main(int argc, char *argv[])
     QSpinBox* magSpin = new QSpinBox;
     magSpin->setSuffix( " мм" );
     magSpin->clear();
-    QHBoxLayout* magHLayout = new QHBoxLayout;
-    magHLayout->addWidget( magCheck );
-    magHLayout->addWidget( magSpin );
-    mainLayout->addLayout( magHLayout, 12, 1, 1, 1 );
 
     //Скорость кровотока
     QLabel* speedBloodLabel = new QLabel( "Скорость кровотока: " );
@@ -176,12 +120,6 @@ int main(int argc, char *argv[])
     speedBloodGroup->addButton( speedBloodDownRadio );
     speedBloodGroup->addButton( speedBloodMidRadio );
     speedBloodGroup->setExclusive( true );
-    QHBoxLayout* speedBloodLayout = new QHBoxLayout;
-    speedBloodLayout->addWidget( speedBloodLabel );
-    speedBloodLayout->addWidget( speedBloodUpRadio );
-    speedBloodLayout->addWidget( speedBloodMidRadio );
-    speedBloodLayout->addWidget( speedBloodDownRadio );
-    mainLayout->addLayout( speedBloodLayout, 13, 1, 1, 1 );
 
     //Печеночные Вены
     QCheckBox* hepaticCheck = new QCheckBox( "Печоночные вены: не изменены" );
@@ -190,7 +128,6 @@ int main(int argc, char *argv[])
                           [&] { if ( !( hepaticCheck->isChecked() ) ) hepaticCheck->setText("Печоночные вены: не изменены");
                                 if ( ( hepaticCheck->isChecked() ) ) hepaticCheck->setText("Печоночные вены: изменены");
                                 });
-    mainLayout->addWidget( hepaticCheck, 14, 1 );
 
     //Нижняя полая вена
     QCheckBox* downVenaCheck = new QCheckBox( "Нижняя полая вена: не расширена, " );
@@ -201,10 +138,6 @@ int main(int argc, char *argv[])
     QSpinBox* downVenaSpin = new QSpinBox;
     downVenaSpin->setSuffix( " мм" );
     downVenaSpin->clear();
-    QHBoxLayout* downVenaHLayout = new QHBoxLayout;
-    downVenaHLayout->addWidget( downVenaCheck );
-    downVenaHLayout->addWidget( downVenaSpin );
-    mainLayout->addLayout( downVenaHLayout, 15, 1, 1, 1 );
 
     //Внутрипеченочные желчные протоки
     QCheckBox* inhepaticCheck = new QCheckBox( "Внутрипечоночные желчные протоки: не расширены" );
@@ -213,7 +146,6 @@ int main(int argc, char *argv[])
                           [&] { if ( !( inhepaticCheck->isChecked() ) ) inhepaticCheck->setText("Внутрипечоночные желчные протоки: не расширены");
                                 if ( ( inhepaticCheck->isChecked() ) ) inhepaticCheck->setText( "Внутрипечоночные желчные протоки:    расширены" );
                                 });
-    mainLayout->addWidget( inhepaticCheck, 16, 1 );
 
     //Общий желчный проток:
     QCheckBox* commonBileCheck = new QCheckBox( "Общий желчный проток: не изменен," );
@@ -229,6 +161,124 @@ int main(int argc, char *argv[])
                       [&] { if ( !( commonBileCheck2->isChecked() ) ) commonBileCheck2->setText(" стенки: не утолщены");
                             if ( ( commonBileCheck2->isChecked() ) ) commonBileCheck2->setText(" стенки:     утолщены");
                             });
+
+
+//ЖЕЛЧНЫЙ ПУЗЫРЬ
+    QLabel* gallbladerLabel = new QLabel( " ЖЕЛЧНЫЙ ПУЗЫРЬ: " );
+
+    //Размер
+    QSpinBox* gallbladerSizeSpin = new QSpinBox;
+    QLabel* gallbladerSizeLabel = new QLabel( "Размер: " );
+
+    //Деформация
+
+    //Стенки
+    QSpinBox* gallbladerWallSpin = new QSpinBox;
+    QLabel* gallbladerWallLabel = new QLabel( "Стенки: " );
+
+    //Содержимое
+    QCheckBox* gallbladedContentCheck = new QCheckBox( "Содержимое: неоднородное" );
+    gallbladedContentCheck->setFixedSize( 230, 20 );
+    QObject::connect( gallbladedContentCheck, &QCheckBox::stateChanged,
+                          [&] { if ( !( gallbladedContentCheck->isChecked() ) ) gallbladedContentCheck->setText("Содержимое: неоднородное" );
+                                if ( ( gallbladedContentCheck->isChecked() ) ) gallbladedContentCheck->setText("Содержимое:   однородное" );
+                                });
+
+    //Конкременты
+    QCheckBox* gallbladerConcrementsCheck = new QCheckBox( "Конкременты: не визуализируются" );
+    gallbladerConcrementsCheck->setFixedSize( 230, 20 );
+    QObject::connect( gallbladerConcrementsCheck, &QCheckBox::stateChanged,
+                          [&] { if ( !( gallbladerConcrementsCheck->isChecked() ) ) gallbladerConcrementsCheck->setText( "Конкременты: не визуализируются" );
+                                if ( ( gallbladerConcrementsCheck->isChecked() ) ) gallbladerConcrementsCheck->setText( "Конкременты:    визуализируются" );
+                                });
+
+
+//ПОДЖЕЛУДОЧНАЯ ЖЕЛЕЗА
+
+
+
+//LAYOUT
+    QGridLayout* mainLayout = new QGridLayout();
+
+    QHBoxLayout* nameLayout = new QHBoxLayout;
+    nameLayout->addWidget( labelName );
+    nameLayout->addWidget( patientName );
+
+    QHBoxLayout* ageLayout = new QHBoxLayout;
+    ageLayout->addWidget( labelAge );
+    ageLayout->addWidget( patientAge );
+
+    QHBoxLayout* dateLayout = new QHBoxLayout;
+    dateLayout->addWidget( labelDate );
+    dateLayout->addWidget( researchDate );
+
+    //Основной слой
+    QFrame mainFrame;
+    mainFrame.setFrameShape( QFrame::Box );
+    mainLayout->addWidget( &mainFrame, 1, 1, 3, 3 );
+
+    mainLayout->addLayout( nameLayout, 1, 1 );
+    mainLayout->addLayout( ageLayout, 2, 1 );
+    mainLayout->addLayout( dateLayout, 3, 1 );
+
+    //Начало протокола:
+    QFrame liverFrame;
+    liverFrame.setFrameShape( QFrame::Box );
+    QLabel* protocallLabel = new QLabel( "\t\t\t ПРОТОКОЛ \n ультразвукогого исследования органов брюшной полости");
+
+
+    mainLayout->addWidget( &liverFrame, 4, 1, 14, 3 );
+    mainLayout->addWidget( protocallLabel, 4, 1);
+    //Заполняеая часть
+    mainLayout->addWidget( liverLabel, 5, 1 );
+    //Расположение печени
+    mainLayout->addLayout( placeLayout, 5, 2, 2, 1);
+    //Доли
+    mainLayout->addWidget( leftShareCheck, 8, 1 );
+    mainLayout->addWidget( rightShareCheck, 9, 1 );
+    //Толщина
+    mainLayout->addWidget( thickLabel1, 8, 2 );
+    mainLayout->addWidget( thickLabel2, 9, 2 );
+    mainLayout->addWidget( leftThickSpin, 8, 3 );
+    mainLayout->addWidget( rightThickSpin, 9, 3 );
+    //Контур
+    QHBoxLayout* contourLayout = new QHBoxLayout;
+    mainLayout->addLayout( contourLayout, 10, 1, 1, 1 );
+    contourLayout->addWidget( contourLabel );
+    contourLayout->addWidget( contourStraight );
+    contourLayout->addWidget( contourNotStraight );
+    contourLayout->addWidget( contourHilly );
+    //Эхоструктура
+    mainLayout->addWidget( echoCheck, 10, 2 );
+    //Эхогенность
+    QHBoxLayout* echogenLayout = new QHBoxLayout;
+    echogenLayout->addWidget( echogenLabel );
+    echogenLayout->addWidget( echogenUpRadio );
+    echogenLayout->addWidget( echogenMidRadio );
+    echogenLayout->addWidget( echogenDownRadio );
+    mainLayout->addLayout( echogenLayout, 11, 1, 1, 1 );
+    //Магистр. Ствол
+    QHBoxLayout* magHLayout = new QHBoxLayout;
+    magHLayout->addWidget( magCheck );
+    magHLayout->addWidget( magSpin );
+    mainLayout->addLayout( magHLayout, 12, 1, 1, 1 );
+    //Скорость кровотока
+    QHBoxLayout* speedBloodLayout = new QHBoxLayout;
+    speedBloodLayout->addWidget( speedBloodLabel );
+    speedBloodLayout->addWidget( speedBloodUpRadio );
+    speedBloodLayout->addWidget( speedBloodMidRadio );
+    speedBloodLayout->addWidget( speedBloodDownRadio );
+    mainLayout->addLayout( speedBloodLayout, 13, 1, 1, 1 );
+    //Печеночные вены
+    mainLayout->addWidget( hepaticCheck, 14, 1 );
+    //Нижняя полая вена
+    QHBoxLayout* downVenaHLayout = new QHBoxLayout;
+    downVenaHLayout->addWidget( downVenaCheck );
+    downVenaHLayout->addWidget( downVenaSpin );
+    mainLayout->addLayout( downVenaHLayout, 15, 1, 1, 1 );
+    //Внутрипеченочные желчные протоки
+    mainLayout->addWidget( inhepaticCheck, 16, 1 );
+    //Общий желчный проток
     QHBoxLayout* commonBileHLayout = new QHBoxLayout;
     commonBileHLayout->addWidget( commonBileCheck );
     commonBileHLayout->addWidget( commonBileSpin );
@@ -236,15 +286,12 @@ int main(int argc, char *argv[])
     mainLayout->addLayout( commonBileHLayout, 17, 1, 1, 2 );
 
 
-//ЖЕЛЧНЫЙ ПУЗЫРЬ
+    //ЖЕЛЧНЫЙ ПУЗЫРЬ
     QFrame* gallbladerFrame = new QFrame;
     gallbladerFrame->setFrameShape( QFrame::Box );
     mainLayout->addWidget( gallbladerFrame, 18, 1, 7, 3 );
-    QLabel* gallbladerLabel = new QLabel( " ЖЕЛЧНЫЙ ПУЗЫРЬ: " );
     mainLayout->addWidget( gallbladerLabel, 18, 1);
     //Размер
-    QSpinBox* gallbladerSizeSpin = new QSpinBox;
-    QLabel* gallbladerSizeLabel = new QLabel( "Размер: " );
     QHBoxLayout* gallbladerSizeLayout = new QHBoxLayout;
     gallbladerSizeSpin->setSuffix(" мм");
     gallbladerSizeSpin->setRange( 0, 300 );
@@ -252,6 +299,18 @@ int main(int argc, char *argv[])
     gallbladerSizeLayout->addWidget( gallbladerSizeLabel );
     gallbladerSizeLayout->addWidget( gallbladerSizeSpin );
     mainLayout->addLayout( gallbladerSizeLayout, 19, 1 );
+    //Стенки
+    QHBoxLayout* gallbladerWallLayout = new QHBoxLayout;
+    gallbladerWallSpin->setSuffix(" мм");
+    gallbladerWallSpin->setRange( 0, 300 );
+    gallbladerWallSpin->clear();
+    gallbladerWallLayout->addWidget( gallbladerWallLabel );
+    gallbladerWallLayout->addWidget( gallbladerWallSpin );
+    mainLayout->addLayout( gallbladerWallLayout, 20, 1 );
+    //Содержимое
+    mainLayout->addWidget( gallbladedContentCheck, 21, 1 );
+    //Конкременты
+     mainLayout->addWidget( gallbladerConcrementsCheck, 22, 1 );
 
 
     wgt.setLayout( mainLayout );
