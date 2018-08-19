a9f7105a99a2b45f1bd30e27c3dafb15e43866cb
paniquex <paniquexx@gmail.com>
2018-08-19 16:19:10 +0500

    patient form to enter the name and age of patient

---
+++ newpatientform.cpp
@@ -0,0 +1,6 @@
+#include "newpatientform.h"
+
+newPatientForm::newPatientForm(QWidget *parent) : QWidget(parent)
+{
+
+}
---
+++ newpatientform.h
@@ -0,0 +1,18 @@
+#ifndef NEWPATIENTFORM_H
+#define NEWPATIENTFORM_H
+
+#include <QObject>
+#include <QWidget>
+
+class newPatientForm : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit newPatientForm(QWidget *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // NEWPATIENTFORM_H
