a5003ea4f4414b75f5c1cebb20bfb1d47493e828
paniquex <paniquexx@gmail.com>
2018-09-20 23:35:09 +0500

    male and female added

--- newpatientform.cpp
+++ newpatientform.cpp
@@ -8,6 +8,16 @@ void newPatientForm::setupUi( QWidget* wgt )
     patientNameLine = new QLineEdit;
     patientAgeLabel = new QLabel( "Дата рождения: " );
     patientAgeDate = new QDateEdit;
+    patientMaleRadio = new QRadioButton( "Муж." );
+    patientFemaleRadio = new QRadioButton( "Жен." );
+    patientSexLabel = new QLabel( "Пол пациента: " );
+    patientGroup = new QButtonGroup;
+    patientGroup->addButton( patientMaleRadio );
+    patientGroup->addButton( patientFemaleRadio );
+    patientGroup->setExclusive( true );
+    patientSexLayout = new QHBoxLayout;
+    patientSexLayout->addWidget( patientMaleRadio );
+    patientSexLayout->addWidget( patientFemaleRadio );
     patientAgeDate->setCalendarPopup( true );
 
     patientFormLayout->addWidget( patientInfoLabel );
@@ -15,6 +25,9 @@ void newPatientForm::setupUi( QWidget* wgt )
     patientFormLayout->addWidget( patientNameLine );
     patientFormLayout->addWidget( patientAgeLabel );
     patientFormLayout->addWidget( patientAgeDate );
+    patientFormLayout->addWidget( patientSexLabel );
+    patientFormLayout->addWidget( patientMaleRadio );
+    patientFormLayout->addWidget( patientFemaleRadio );
 
     wgt->setLayout( patientFormLayout );
 
